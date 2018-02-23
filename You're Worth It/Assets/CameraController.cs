5a6606f5be395405831365fc0485c39e9bd22059
tom-bagstevold <tbagstevold@gmail.com>
2018-02-23 16:41:59 -0500

    Project

---
+++ You're Worth It/Assets/CameraController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    public WebCamTexture mCamera = null;
+    public GameObject plane;
+
+    // Use this for initialization
+    void Start()
+    {
+        Debug.Log("Script has been started");
+        plane = GameObject.FindWithTag("Player");
+
+
+        mCamera = null;
+        WebCamDevice[] wdcs = WebCamTexture.devices;
+        for (int n = 0; n < wdcs.Length; ++n)
+        {
+            if (wdcs[n].isFrontFacing)
+            {
+                mCamera = new WebCamTexture(wdcs[n].name);
+            }
+        }
+
+        if (mCamera == null)
+        {
+            mCamera = new WebCamTexture();
+        }
+
+
+
+       // mCamera = new WebCamTexture();
+        plane.GetComponent<Renderer>().material.mainTexture = mCamera;
+        mCamera.Play();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
